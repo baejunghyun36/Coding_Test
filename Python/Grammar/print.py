26ad271503621f315ba3affb2908b77f64cb4ac1
baejunghyun36 <bae1004kin@gmail.com>
2022-03-27 18:30:53 +0900

    .

---
+++ Python/Grammar/print.py
@@ -0,0 +1,10 @@
+#산수
+print(1)
+print(-10)
+print(3.14)
+print(1000)
+print(2*8)
+print(1*(2/8))
+print(1)
+
+
---
+++ Python/Grammar/string.py
@@ -0,0 +1,5 @@
+# 문자열 
+print('풍성')
+print("나비")
+print("zzzzz")
+print("zzz"*9) #문자열 곱해짐
