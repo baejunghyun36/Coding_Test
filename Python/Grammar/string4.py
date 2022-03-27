845e1547d6649a0aa1eaefa6773fe1cfc12bd178
baejunghyun36 <bae1004kin@gmail.com>
2022-03-27 21:02:57 +0900

    .

--- Python/Grammar/string4.py
+++ Python/Grammar/string4.py
@@ -1,20 +1,23 @@
 python = "Python is Amazing"
-print(python.lower())
-print(python.upper())
-print(python[0].isupper())
-print("문자열 길이",len(python))
 
-print(python.replace("Python", "Java"))
+print(python.lower()) #소문자로
+print(python.upper()) #대문자로
+print(python[0].isupper()) #첫번째 글자 대문자인지. True
+print("문자열 길이",len(python)) #문자열 길이
+
+print(python.replace("Python", "Java")) #Python문자열을 Java로 치환
 print(python)
-index = python.index("n") # 5번째 
+
+index = python.index("n") # n의 인덱스 번호 5
 print(index) 
+
 index = python.index("n", index+1) #5번째 이후부터 찾는거 
 print(index)
 
-index= print(python.find("is"))
+print(python.find("is")) # "is" 문자열 찾고 시작 인덱스 반환
 print(python.find("Java")) #원하는 값이 없으면 -1 반환
-#print(python.index("Java"))#에러 
-print("hi")
+#print(python.index("Java"))#해당 문자열 없으면 에러 
+
 print(python.count("n"))  #n이 몇번 나왔는지 
 
 
--- Python/Grammar/stringEscape.py
+++ Python/Grammar/stringEscape.py
@@ -1,18 +1,13 @@
-
-
 print("백문이 불여일견 \n백견이 불여일타")
 
-
-print("저는'나도코딩'입니다.")
-
-print("저는 \"나도코딩\"입니다") #따옴표 처리
-print("저는 \'나도코딩\'입니다")
+print("저는 \"배정현\"입니다") #\"큰 따옴표 처리
+print("저는 \'배정현\'입니다") #\'작은 따옴표 처리
 
 #\\ : 문장 내에서 \
 print(" \\usr\\")
 
 #\r : 커서를 맨 앞으로 이동 
-print("Red  Apple\rPine")
+print("RedApple\rPine") #Pine 문자열을 맨앞에 덮기
 
 # \b : 백스페이스 (한 글자 삭제)
 print("Redd\bApple")
