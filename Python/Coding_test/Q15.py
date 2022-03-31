eeb88d847e45900e7b07db0caaeb1f407b67a86f
baejunghyun36 <bae1004kin@gmail.com>
2022-03-31 19:26:21 +0900

    python

--- Python/Coding_test/Q15.py
+++ Python/Coding_test/Q15.py
@@ -1,28 +1,7 @@
-from collections import deque
+n = int(input())
+data = list(map(int, input().split()))
+add, sub, mul, div = map(int, input().split())
 
+min_value = 1e9 
+max_value = -1e9
 
-n,m,k,x = map(int, input().split())
-graph = [[] for _ in range (n+1)]
-
-for _ in range(m):
-    a, b = map(int, input().split())
-    graph[a].append(b)
-
-distance = [-1]*(n+1)
-distance[x] = 0
-
-q = deque([x])
-while q : 
-    now = q.popleft()
-    for next_node in graph[now]:
-        if distance[next_node]==-1:
-            distance[next_node] = distance[now]+1
-            q.append(next_node)
-
-check = False
-for i in range(1, n+1):
-    if distance[i]==k:
-        print(i)
-        cehck = True
-if check == True:
-    print(-1)
