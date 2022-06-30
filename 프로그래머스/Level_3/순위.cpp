b09dd6d84306cc4199aff9db23e3e72542f4b79c
baejunghyun36 <bae1004kin@gmail.com>
2022-06-30 11:31:30 +0900

    .

---
+++ 프로그래머스/Level_3/순위.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, vector<vector<int>> results) {
+    int answer = 0;
+    vector<vector<bool>> graph(n+1, vector<bool>(n+1,false)); 
+    for(auto x : results) graph[x[0]][x[1]]=true; 
+    for(int k=1; k<=n; k++){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if(graph[i][k]&graph[k][j])graph[i][j]=true; 
+            }
+        }
+    }
+    for(int i=1; i<=n; i++){
+        int cnt =0; 
+        for(int j=1; j<=n; j++){
+            if(graph[i][j]||graph[j][i])cnt++; 
+        }
+        if(cnt==n-1)answer++; 
+    }    
+    return answer;
+}
